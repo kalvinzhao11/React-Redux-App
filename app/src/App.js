ad9ee66679d36e5a8ddd9afcea579981565728d6
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-09-09 21:13:40 -0400

    added styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,12 @@
 import React, { useEffect } from 'react';
 import {connect} from 'react-redux'
 import {fetchDOG} from './store/actions/dogActions'
+import styled from 'styled-components'
+import './App.css'
+
+// const Div = styled.div `
+//   text-align: center;
+// `
 
 import Dog from './components/Dog'
 function App({fetchDOG, loading, errorMessage}) {
@@ -11,7 +17,7 @@ function App({fetchDOG, loading, errorMessage}) {
   return (
     <div className="App">
       <h1>Random Dog Image</h1>
-      {/* {!loading ? <Dog /> : <div>... loading</div>} */}
+      {/* {!loading ? <Dog /> : <div>... loadingg</div>} */}
       {!errorMessage ? (!loading ? <Dog /> : <div>... loading</div>) : <div>{errorMessage}</div>}
     </div>
   );
